2405fb9fe6b3500ac02a0cea685608ea0cdd96f7
Nelly Smetanina <sm.nelly1@yandex.ru>
2023-12-13 23:35:32 +0300

    'This is my first project commit for real'

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # qa_python
+1.The method checks that the dictionary with genres returns the correct number of books 
+test_add_new_book_add_two_books
+2.Checking that the genre of the added book exists
+test_add_new_book_add_new_book
+3.Checking that the genre of the added book matches the title of the book 
+test_set_book_genre_add_genre
+4.We got the genre of the book by name
+test_get_book_genre_output_genre
+5.we got the added genre of the book 
+test_get_books_with_specific_genre_output_specific_genre
+6.We got a dictionary with the genres of books
+test_get_books_genre_current_dictionary
+7.We received books for children whose genre does not fit the age rating
+test_get_books_for_children_rating_book_kids
+8.The book has been added to favorites. The title of the book corresponds to the one added
+test_add_book_in_favorites_add_this_book_favorite
+9.If the title of the book is not in the favorites, we receive a message
+test_delete_book_from_favorites_del_this_book_favorite
+10.we get the title of the book in favorites
+test_get_list_of_favorites_books_get_one_favorite_book
--- tests.py
+++ tests.py
@@ -1,24 +1,56 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
+        collector.add_new_book('Tintin')
+        collector.add_new_book('Под куполом')
+        assert len(collector.get_books_genre()) == 2
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_add_new_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Убийство в Восточном экспрессе')
+        assert 'Убийство в Восточном экспрессе' in collector.books_genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_add_genre(self):
+        collector = BooksCollector()
+        collector.set_book_genre('Tintin', 'Мультфильмы')
+        collector.set_book_genre('Убийство в Восточном экспрессе', 'Детективы')
+        collector.set_book_genre('Под куполом', 'Ужасы')
+        assert {'Tintin': 'Мультфильмы'}
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_book_genre_output_genre(self):
+        collector = BooksCollector()
+        collector.get_book_genre('Убийство в Восточном экспрессе')
+        assert {'Убийство в Восточном экспрессе': 'Детективы'}
+
+    def test_get_books_with_specific_genre_output_specific_genre(self):
+        collector = BooksCollector()
+        collector.get_books_with_specific_genre('Детективы')
+        assert 'Детективы' in collector.genre_age_rating
+
+    def test_get_books_genre_current_dictionary(self):
+        collector = BooksCollector()
+        assert collector.get_books_genre
+
+    def test_get_books_for_children_rating_book_kids(self):
+        collector = BooksCollector()
+        collector.get_books_for_children()
+        assert collector.get_books_for_children() != ['Ужасы', 'Детективы']
+
+    def test_add_book_in_favorites_add_this_book_favorite(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Убийство в Восточном экспрессе')
+        assert collector.add_book_in_favorites('Убийство в Восточном экспрессе') != ['Tintin']
+
+    def test_delete_book_from_favorites_del_this_book_favorite(self):
+        collector = BooksCollector()
+        collector.delete_book_from_favorites('Девушка в поезде')
+        assert (collector.delete_book_from_favorites('Девушка в поезде'), 'Этой книги нет в списке')
+
+    def test_get_list_of_favorites_books_get_one_favorite_book(self):
+        collector = BooksCollector()
+        collector.get_list_of_favorites_books()
+        assert 'Убийство в Восточном экспрессе'
